0ec078e6175925fd7636bfe3479c32ea38447e3e
schhidal23 <chhidalsandip@gmail.com>
2025-11-06 19:57:45 +0100

    Lecture 5 Tests

---
+++ Tests/bandex.m
@@ -0,0 +1,14 @@
+% bandex.m design a bandpass filter and plot frequency response
+
+%% fbe =[0 0.24 0.26 0.74 0.76 1];  % frequency band edges as a fraction of 
+fbe = [0, 0.24, 0.26, 0.59, 0.595, 0.605, 0.61, 0.74, 0.76, 1]                               %   the Nyquist frequency
+%% damps =[0 0 1 1 0 0];            % desired amplitudes at band edges
+damps = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0]
+fl=59;                          % filter size
+b=firpm(fl,fbe,damps);          % b is the designed impulse response
+figure(1)
+freqz(b)                        % plot frequency response to check design
+
+[b,a]=cheby1(10,5,0.25);
+figure(2)
+freqz(b,a)
